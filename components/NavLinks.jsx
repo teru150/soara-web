8e1e6c4054d9f0225fa7359ba003a5932566f18c
fishnos <dkny11692@gmail.com>
2025-07-22 00:54:18 -0400

    Initial changes

---
+++ components/NavLinks.jsx
@@ -0,0 +1,16 @@
+"using client";
+
+export default function NavLinks() {
+    const paths = [
+        {
+            path: "/",
+            name: "home",
+        }
+    ]
+
+    return (
+        <div className="gap-2 flex flex-col h-auto w-auto">
+            
+        </div>
+    );
+}
